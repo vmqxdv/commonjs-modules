db33ba86fb4230a832c2fdede62bbae056aa6640
vmqxdv <vquintilio@gmail.com>
2025-03-24 13:28:50 +0100

    feat: Creata funzione ed esportata

---
+++ hobbies.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ * 1. Crea una funzione che accetta tre parametri: hobbyOne, hobbyTwo, hobbyThree. La funzione dovrebbe restituire  un array di stringhe contenenti gli hobby.
+ * 2. Esporta la funzione dal file.
+ * 
+ */
+
+function createArray(hobbyOne, hobbyTwo, hobbyThree) {
+  return [hobbyOne, hobbyTwo, hobbyThree];
+};
+
+export default createArray;
