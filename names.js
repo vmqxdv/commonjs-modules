3ab4bf956f9c6c8b209ab7c91572fcf2e63220b6
vmqxdv <vquintilio@gmail.com>
2025-03-24 13:23:57 +0100

    first commit

---
+++ names.js
@@ -0,0 +1,7 @@
+/**
+ * 
+ * 1. Crea una funzione che accetta due parametri: firstName, lastName. La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName.
+ * 2. Esporta la funzione dal file.
+ * 
+ */
+
