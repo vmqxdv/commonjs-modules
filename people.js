1fd09534861c8437b1e2e2251e5d49de70a76722
vmqxdv <vquintilio@gmail.com>
2025-03-24 13:34:36 +0100

    feat: Importate funzioni precedenti e creata funzione finale

---
+++ people.js
@@ -0,0 +1,20 @@
+/**
+ * 
+ * 1. Importa la tua funzione da names.js
+ * 2. Importa la tua funzione da hobbies.js
+ * 3. Crea una funzione che non ha parametri. La funzione dovrebbe restituire un oggetto con due proprietà:
+ * fullName ed hobbies. All'interno della tua funzione, usa le tue due funzioni precedenti per costruire l'oggetto.
+ * 
+ */
+
+import createObj from './names.js';
+import createArray from './hobbies.js';
+
+function userData() {
+  const { firstName, lastName } = createObj('Valerio', 'Quintilio');
+  const hobbies = createArray('Cooding', 'Giochi', 'Palestra');
+
+  return { fullName: `${firstName} ${lastName}`, hobbies }
+};
+
+console.log(userData());
